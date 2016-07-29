7adb833f2fcf7ad1cdb77955c63de169be707f6d
Ville Väänänen <ville.vaananen@lucify.com>
2016-07-29 16:46:30 +0300

    Make startup more robust

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,16 @@
 
 conf=/etc/gitlab-runner/config.toml
 
+gettoken () { psql -q -A -t -c 'select runners_registration_token from application_settings;'; }
+
 if [ "$1" = 'gitlab-runner'  ] && [ ! -f "$conf" ]; then
+
+  if [[ -z "$DEBUG" ]] ; then
+    exec 3>&1 &>/dev/null
+  else
+    exec 3>&1
+  fi
+
   host="${PGHOST:-postgresql}"
   user="${PGUSER:-gitlab}"
   password="${PGPASSWORD:-password}"
@@ -15,16 +24,16 @@ if [ "$1" = 'gitlab-runner'  ] && [ ! -f "$conf" ]; then
   export PGPASSWORD="$password"
   export PGDATABASE="$db"
 
-  token=""
   min_length=15
+  token=$(gettoken)
 
-  until [ "${#token}" -gt "$min_length" ] && gitlab-runner register -n -r $token >/dev/null 2>&1; do
-    echo "Failed to register - sleeping"
+  until [ "${#token}" -gt "$min_length" ] && gitlab-runner register -n -r $token; do
+    echo "Failed to register - sleeping" >&3
     sleep 5
-    token=`psql -q -A -t -c 'select runners_registration_token from application_settings;' 2>/dev/null`
+    token=$(gettoken)
   done
 
-  echo "Runner registered succesfully"
+  echo "Runner registered succesfully" >&3
 
 fi
 
