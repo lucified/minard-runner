678dea9d1d44fb331afc5d131d73a2026e969a01
Ville Väänänen <ville.vaananen@lucify.com>
2016-10-02 04:31:10 +0300

    Enable CircleCI production deployments

---
+++ ecs-updater.js
@@ -0,0 +1,13 @@
+
+const env = process.env.LUCIFY_ENV === 'production' ? 'production' : 'staging';
+
+module.exports = {
+  REGION: 'eu-west-1',
+  CLUSTER: 'minard',
+  SERVICE: `minard-runner-${env}`,
+  CONTAINER: 'runner',
+  IMAGE: 'runner',
+  BUCKET: 'lucify-configuration',
+  KEY: `ecs_services/runner_${env}`,
+  DOCKERFILE: 'Dockerfile-lucify',
+};
